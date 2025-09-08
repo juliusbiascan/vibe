a08412cd50aa499de9aa0f6ba8543680e67a4124
juliusbiascan <juliusbiascan.bscs.pass@gmail.com>
2025-09-08 15:15:49 +0800

    05: ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import React from "react";
 import { toast } from "sonner";
 
 const Page = () => {
-
+  const [value, setValue] = React.useState("");
   const trpc = useTRPC();
 
   const invoke = useMutation(
@@ -18,12 +20,16 @@ const Page = () => {
   )
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <Button
         disabled={invoke.isPending}
         onClick={() => {
           invoke.mutate(
             {
-              text: "Run Background Jobs",
+              value: value,
             }
           );
         }}>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,18 +1,23 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
+  async ({ event }) => {
 
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer.  You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: gemini({ model: "gemini-1.5-flash" }),
+    });
 
-    //Imagine this a transcript step
-    await step.sleep("wait-a-moment", "10s");
+    const { output } = await codeAgent.run(
+      'Write the following snippet: ' + event.data.value,
+    );
 
-    //Imagine this a summary step
-    await step.sleep("wait-a-moment", "5s");
-
-
-    return { message: `Hello ${event.data.email}!` };
+    return { output };
   },
+
 );
