080ae7e39431da20437a5af891b3f70bcbf967de
Julius Biascan <juliusbiascan.bscs.pass@gmail.com>
2025-09-08 16:22:32 +0800

    06: e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,18 @@
+import { Sandbox } from "@e2b/code-interpreter";
+
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
 
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
@@ -17,7 +25,15 @@ export const helloWorld = inngest.createFunction(
       'Write the following snippet: ' + event.data.value,
     );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+
+      const host = sandbox.getHost(3000);
+
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   },
 
 );
